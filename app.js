c7be39028045c56eaeafd04786886536970b2a80
Arpan Patel <ap.patel1893@gmail.com>
2019-04-30 23:10:45 -0500

    use yargs package

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-const validator = require('validator')
 
-const getNotes = require('./notes.js/index.js')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-const msg = getNotes();
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('Title:' + argv.title)
+    }
+})
 
-console.log(validator.isURL('https://swamiji.com'));
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function (){
+        console.log('removing a new note!')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    handler: function (){
+        console.log('listing a note!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function (){
+        console.log('reading a new note!')
+    }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv);
+
