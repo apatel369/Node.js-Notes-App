e9e46a63ef2b173f64e383fc328b5f81325df72a
Arpan Patel <ap.patel1893@gmail.com>
2019-05-01 20:38:04 -0500

    finish add command

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 yargs.command({
     command: 'add',
@@ -19,7 +19,7 @@ yargs.command({
         },
     },
     handler: function (argv){
-        console.log('Title:' + argv.title + ' Body:' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
